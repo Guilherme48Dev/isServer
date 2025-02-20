8e40e58e6eccd257dadf0ce057212b28407344db
Guilherme48Dev <guilhermesouzadjesusdev@gmail.com>
2025-02-20 03:12:56 -0300

    favoritos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const rotaLivro = require("./rotas/livro");
+const rotaFavorito = require("./rotas/favorito");
 const cors = require("cors");
-
 
 const app = express();
 app.use(express.json());
@@ -10,6 +10,7 @@ app.use(cors({ origin: "*" }));
 const port = 8000;
 
 app.use("/livros", rotaLivro);
+app.use("/favoritos", rotaFavorito);
 
 app.listen(port, () => {
   console.log(`Escutando a porta ${port}`);
--- rotas/livro.js
+++ rotas/livro.js
@@ -5,11 +5,8 @@ const router = Router();
 
 router.get("/", getLivros);
 router.get("/:id", getLivro);
-
 router.post("/", postLivro)
-
 router.patch('/:id', patchLivro)
-
 router.delete("/:id", deleteLivro)
 
 module.exports = router;
